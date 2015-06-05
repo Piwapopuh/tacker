82fde8dc533bfdb3cba7f661cd8831a5ed6751a5
Thomas Dräger <td7650@gmx.de>
2015-06-05 14:53:26 +0200

    normalize key and values in parameters

--- src/Tacker/functions.php
+++ src/Tacker/functions.php
@@ -4,7 +4,9 @@ function tacker_array_map_recursive(array $array, $f) {
     $map = array();
 
     foreach ($array as $k => $v) {
-        $map[$k] = is_array($v) ? tacker_array_map_recursive($v, $f) : call_user_func($f, $v);
+        $k = is_array($k) ? tacker_array_map_recursive($k, $f) : call_user_func($f, $k);
+		$v = is_array($v) ? tacker_array_map_recursive($v, $f) : call_user_func($f, $v);
+		$map[$k] = $v;
     }
 
     return $map;
